5d006a639f8474eb39e7cb0e47b6f43098ff6eaf
luis.rodas <luis.rodas@digitalvaultstudio.com>
2020-04-12 15:02:09 -0700

    adding main entry point

---
+++ src/components/main.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import { Color } from 'ink';
+
+export class Main extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			i: 0
+		};
+	}
+
+	render() {
+		return <Color green>{this.state.i} tests passed</Color>;
+	}
+
+	componentDidMount() {
+		this.timer = setInterval(() => {
+			this.setState({
+				i: this.state.i + 1
+			});
+		}, 100);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timer);
+	}
+}
