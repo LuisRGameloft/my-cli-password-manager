6c88ddeaafac24b6db80cdfc867a9fc05d1e300f
luis.rodas <luis.rodas@digitalvaultstudio.com>
2020-04-13 22:21:25 -0700

    use menu component

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,27 +1,26 @@
 import React, {Component} from 'react';
 import { Color } from 'ink';
+import { Menu } from './menu'
 
 export class Main extends Component {
 	constructor() {
 		super();
-		this.state = {
-			i: 0
-		};
 	}
 
 	render() {
+		if(this.props.database === 'none') {
+			return ( 
+				<Menu /> 
+			);
+		}
 		return <Color green>{this.state.i} {this.props.database} passed</Color>;
 	}
 
 	componentDidMount() {
-		this.timer = setInterval(() => {
-			this.setState({
-				i: this.state.i + 1
-			});
-		}, 100);
+	
 	}
 
 	componentWillUnmount() {
-		clearInterval(this.timer);
+		
 	}
 }
